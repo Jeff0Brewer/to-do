2352935bbf673a5425a5bd93c9478d9c8322fee5
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-10-23 10:59:06 -0600

    init item component

---
+++ components/item.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react'
+import styles from '../styles/Item.module.css'
+
+type ItemProps = {
+    text: string,
+    setText: (value: string) => void,
+    completed: boolean,
+    setCompleted: (value: boolean) => void
+}
+
+const Item: FC<ItemProps> = props => {
+    const toggleCompleted = (e: React.ChangeEvent<HTMLInputElement>) => {
+        props.setCompleted(e.target.checked)
+    }
+
+    const changeText = (e: React.ChangeEvent<HTMLInputElement>) => {
+        props.setText(e.target.value)
+    }
+
+    return (
+        <div className={styles.itemContainer}>
+            <input type="checkbox" defaultChecked={props.completed} onChange={toggleCompleted}/>
+            <input type="text" defaultValue={props.text} onChange={changeText} />
+        </div>
+    )
+}
+
+export default Item
