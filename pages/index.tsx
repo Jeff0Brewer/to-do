bf18c5d03e6f207b1538610912c17eb9f035f74a
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-11-09 15:35:07 -0700

    remove test data from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,81 +3,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import ListInterface from '../components/list-interface'
 import List from '../components/list'
+import { getBlankList } from '../lib/list-util'
 import { ListData } from '../lib/types'
-import { getKey } from '../lib/key'
-
-const testLists: Array<ListData> = [
-    {
-        title: 'list 0',
-        date: new Date(),
-        items: [
-            {
-                text: '1sdf',
-                completed: true,
-                children: [],
-                key: getKey()
-            }, {
-                text: '345f',
-                completed: false,
-                children: [
-                    {
-                        text: 'fdsa',
-                        completed: true,
-                        children: [],
-                        key: getKey()
-                    }
-                ],
-                key: getKey()
-            }, {
-                text: 'mhmmmmm',
-                completed: false,
-                children: [
-                    {
-                        text: 'a',
-                        completed: false,
-                        children: [],
-                        key: getKey()
-                    }, {
-                        text: 'b',
-                        completed: true,
-                        children: [],
-                        key: getKey()
-                    }, {
-                        text: 'c',
-                        completed: false,
-                        children: [
-                            {
-                                text: 'd',
-                                completed: false,
-                                children: [],
-                                key: getKey()
-                            }
-                        ],
-                        key: getKey()
-                    }
-                ],
-                key: getKey()
-            }
-        ],
-        key: getKey()
-    },
-    {
-        title: 'list 1',
-        date: new Date(),
-        items: [
-            {
-                text: 'mdmd',
-                completed: false,
-                children: [],
-                key: getKey()
-            }
-        ],
-        key: getKey()
-    }
-]
 
 const Home: NextPage = () => {
-    const [lists, setLists] = useState<Array<ListData>>(testLists)
+    const [lists, setLists] = useState<Array<ListData>>([getBlankList()])
     const [listInd, setListInd] = useState<number>(0)
     return (
         <>
