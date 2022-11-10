bc3c4f52afc3a6e1ab1f579b4f70dc0bcadbfb61
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-11-10 16:32:04 -0700

    init nextauth obj

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,14 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import prisma from '../../../prisma/prisma-client'
+import prisma from '../../../prisma/client'
+
+export default NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: '',
+            clientSecret: ''
+        })
+    ]
+})
