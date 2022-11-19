705da4a9f0079ade7780da443a23ac5ca8e230eb
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-11-19 12:06:31 -0700

    init sign in style

--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -1,9 +1,16 @@
 import React, { FC } from 'react'
 import { signIn } from 'next-auth/react'
+import styles from '../styles/SignIn.module.css'
 
 const SignIn: FC<Record<string, never>> = () => {
     return (
-        <button onClick={() => signIn()}>sign in</button>
+        <section className={styles.center}>
+            <div className={styles.wrap}>
+                <h1 className={styles.header}>To Do</h1>
+                <p className={styles.subheader}>A minimal to do list app.</p>
+                <button className={styles.signIn} onClick={() => signIn('google')}>log in</button>
+            </div>
+        </section>
     )
 }
 
